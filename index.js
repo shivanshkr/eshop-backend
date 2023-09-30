3cf953cdb80802fa8469bf72667e7b2b2139a3d6
shivanshkr <shivansh.dbg@gmail.com>
2023-09-30 18:28:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const api = process.env.API_URL
 app.use(`${api}/categories`, categoriesRoutes)
 app.use(`${api}/products`, productsRoutes)
 app.use(`${api}/users`, usersRoutes)
+app.use(`users`, usersRoutes)
 app.use(`${api}/orders`, ordersRoutes)
 
 //Database
